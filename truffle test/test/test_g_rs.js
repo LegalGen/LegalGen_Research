513c7af2779c31a86f1ed2fa3fe7cd251b1a50b7
LegalGen <huyuxin24+LegalGen@gmail.com>
2024-11-26 20:13:46 -0700

    Update test_g_rs.js

--- truffle test/test/test_g_rs.js
+++ truffle test/test/test_g_rs.js
@@ -21,19 +21,15 @@ contract("g_rs", (accounts) => {
   });
 
   it("should test payment functionality", async () => {
-    const paymentAmount = web3.utils.toWei("1", "ether"); // Example payment amount
+    const paymentAmount = web3.utils.toWei("1", "ether"); 
     await contract.pay_0({ from: buyer, value: paymentAmount });
-
-    // Check if the payment has been processed, you can add assertions for the emitted event
-    // Example: Check if state has changed to Locked, or if event Payed was emitted
+    
   });
 
   it("should allow termination of the contract", async () => {
     const buyerIndex = 0;
     await contract.terminateByTransfer(buyerIndex, { from: seller });
-
-    // Add checks here, for example, check if the state is Inactive
-    // Example: Check if state has changed to Inactive or if Terminated event was emitted
+    
    });
   });
 
